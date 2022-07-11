6bde0fc7750cf463d3fe8862e3b171a27d101efe
ahmedhosam283843 <ahmedhosam283843@gmail.com>
2022-07-11 13:38:18 +0200

    Added Live Data

--- app/src/main/java/com/example/myapplication/Fragment3.kt
+++ app/src/main/java/com/example/myapplication/Fragment3.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_3.*
@@ -22,11 +24,23 @@ class Fragment3 : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_3, container, false)
         val viewModel  = ViewModelProvider(this).get(MainClassViewModel::class.java)
-        view.clickNumber.text = viewModel.number.toString()
-        view.button.setOnClickListener {
-            viewModel.addNumber()
-            view.clickNumber.text = viewModel.number.toString()
-
+        viewModel.seconds().observe(viewLifecycleOwner, Observer {
+            view.clickNumber.text = it.toString() })
+        viewModel.isFinished().observe(viewLifecycleOwner, Observer{
+            if(it)
+            Toast.makeText(activity, "Finished", Toast.LENGTH_SHORT).show()
+        })
+        view.start_btn.setOnClickListener {
+            if(view.milliSeconds.text.isEmpty() || view.milliSeconds.text.length <4)
+                Toast.makeText(activity, "Invalid Number", Toast.LENGTH_SHORT).show()
+            else{
+                viewModel.timerValue.value = milliSeconds.text.toString().toLong()
+                viewModel.startTimer()
+            }
+        }
+        view.stop_btn.setOnClickListener {
+            clickNumber.text = "0"
+            viewModel.stopTimer()
         }
 
         return view
--- app/src/main/java/com/example/myapplication/MainClassViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainClassViewModel.kt
@@ -1,10 +1,39 @@
 package com.example.myapplication
 
+import android.content.IntentSender
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainClassViewModel : ViewModel(){
-    var number = 0
-    fun addNumber(){
-        number++
+    private lateinit var timer: CountDownTimer
+    private val _seconds  = MutableLiveData<Int>()
+    private var finished = MutableLiveData<Boolean>()
+    var timerValue = MutableLiveData<Long>()
+
+
+    fun isFinished() : LiveData<Boolean>{
+        return finished
+    }
+    fun seconds():LiveData<Int>{
+        return _seconds
+    }
+    fun startTimer(){
+        timer = object : CountDownTimer(timerValue.value!!.toLong(), 1000){
+            override fun onTick(p0: Long) {
+                val timeLeft = p0/1000
+                _seconds.value = timeLeft.toInt()
+
+            }
+
+            override fun onFinish() {
+                finished.value = true
+            }
+
+        }.start()
+    }
+    fun stopTimer(){
+        timer.cancel()
     }
 }
