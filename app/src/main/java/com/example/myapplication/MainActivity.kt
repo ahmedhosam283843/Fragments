a8f3381e1d7c7e363b6744a64876cb4222c70fd2
ahmedhosam283843 <ahmedhosam283843@gmail.com>
2022-07-17 09:22:14 +0200

    Retrofit with RV

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,55 +1,51 @@
 package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment1.*
-import kotlinx.android.synthetic.main.fragment_2.*
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ItemTodoBinding
+import retrofit2.HttpException
+import retrofit2.http.HTTP
+import java.io.IOException
 
-class MainActivity : AppCompatActivity()/*, Communicator */{
-    lateinit var s1 : Array<String>
-    lateinit var s2 : Array<String>
-    val imgs = listOf<Int>(
-        androidx.customview.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.abc_ab_share_pack_mtrl_alpha,
-    androidx.viewpager.R.drawable.notification_bg, androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_ic_go_search_api_material,
-        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.transition.R.drawable.abc_btn_check_to_on_mtrl_000, androidx.constraintlayout.widget.R.drawable.abc_spinner_textfield_background_material,
-        androidx.constraintlayout.widget.R.drawable.notification_bg, androidx.appcompat.R.drawable.abc_edit_text_material)
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        s1 = resources.getStringArray(R.array.programming_languages)
-        s2 = resources.getStringArray(R.array.description)
-
-
-        val adapter = Adapter(this, s1, s2, imgs)
-
-        recycleView.adapter = adapter
-        recycleView.layoutManager  = LinearLayoutManager(this)
-
-
-//      val frag1  = Fragmnet1()
-//        replaceFragment(frag1)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpRecyclerView()
+        lifecycleScope.launchWhenCreated {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitInstance.api.getTodos()
+            }
+            catch (e: IOException){
+                Log.e("MainActivity", "Network error")
+                return@launchWhenCreated
+            }
+            catch (e: HttpException){
+                Log.e("MainActivity", "Http error")
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null){
+                todoAdapter.todos =  response.body()!!
+            }
+            else{
+                Log.e("MainActivity", "Error: ${response.code()}")
+            }
+            binding.progressBar.isVisible = false
+        }
+    }
+    private fun setUpRecyclerView() = binding.rvTodos.apply{
+        todoAdapter = TodoAdapter()
+        adapter = todoAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
     }
 
 
-//    fun createBundle(txt: String): Bundle{
-//        val bundle = Bundle()
-//        bundle.putString("message",txt)
-//        return bundle
-//    }
-//
-//    fun createFrag(editTextInput: String, fragment: Fragment):Fragment{
-//        val bundle = createBundle(editTextInput)
-//        fragment.arguments = bundle
-//        return fragment
-//    }
-//    fun replaceFragment(fragment: Fragment){
-//        supportFragmentManager.beginTransaction().replace(R.id.frag, fragment).commit()
-//    }
-//
-//    override fun replaceFragmentWithData(editTextInput: String, fragment: Fragment) {
-//        var modifiedFrag = createFrag(editTextInput, fragment)
-//        replaceFragment(modifiedFrag)
-//    }
 }
---
+++ app/src/main/java/com/example/myapplication/TodoApi.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TodoApi {
+    @GET("todos")
+    suspend fun getTodos(): Response<List<Todo>>
+}
