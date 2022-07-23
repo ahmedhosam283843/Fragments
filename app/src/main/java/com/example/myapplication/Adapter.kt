d76e4edbee75f953ffd055d85a382cc91060a50d
ahmedhosam283843 <ahmedhosam283843@gmail.com>
2022-07-23 21:14:55 +0200

    Setup RecyclerView

--- app/src/main/java/com/example/myapplication/Adapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter.kt
@@ -6,39 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.data.User
 import kotlinx.android.synthetic.main.my_row.*
 import kotlinx.android.synthetic.main.my_row.view.*
 
-class Adapter (val ct: Context, var s1: Array<String>, var s2: Array<String>, val imgs: List<Int>): RecyclerView.Adapter<Adapter.MyViewHolder>() {
+class listAdapter (): RecyclerView.Adapter<listAdapter.MyViewHolder>() {
+    private var userList = emptyList<User>()
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView ) {
-        val myText1 = itemView.lang_name
-        val myText2 = itemView.langDescription
-        val imageView = itemView.langImage
-        val rowLayout = itemView.rowLayout
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater  = LayoutInflater.from(ct)
-        val view  = inflater.inflate(R.layout.my_row, parent, false)
-        return MyViewHolder(view)
-     }
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.my_row, parent, false)
+        )
+    }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.imageView.setImageResource(imgs[position])
-        holder.myText1.setText(s1[position])
-        holder.myText2.setText(s2[position])
-        holder.rowLayout.setOnClickListener{
-            var intent = Intent(ct, language::class.java)
-            intent.putExtra("data1", s1[position])
-            intent.putExtra("data2", s2[position])
-            intent.putExtra("image", imgs[position])
-            ct.startActivity(intent)
-        }
+        val user = userList[position]
+        holder.itemView.firstTextView.text = user.firstName
+        holder.itemView.secondTextView.text = user.lastName
+        holder.itemView.ageTextView.text = user.age.toString()
+        holder.itemView.idTextView.text = user.id.toString()
+
+    }
+    fun setData(userList: List<User>) {
+        this.userList = userList
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
-           return s1.size
+        return userList.size
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/list/ListFragment.kt
+++ app/src/main/java/com/example/myapplication/list/ListFragment.kt
@@ -1,24 +1,34 @@
 package com.example.myapplication.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.R
-
+import com.example.myapplication.data.UserViewModel
+import com.example.myapplication.listAdapter
 import kotlinx.android.synthetic.main.fragment1.view.*
 
 
 class ListFragment : Fragment() {
-
+    private lateinit var mViewModel: UserViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment1, container, false)
+        val recyclerView = view.recyclerView
+        val adapter = listAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        mViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+        mViewModel.readAllData.observe(viewLifecycleOwner) { user ->
+            adapter.setData(user)
+        }
         view.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
